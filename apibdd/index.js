d7af7ead9ec6dabf3543624588a79de6a2e8d131
aphelionxd <r.batista@aluno.ifsp.edu.br>
2023-10-16 15:12:51 -0300

    instalação de api para banco de dados

--- apibdd/index.js
+++ apibdd/index.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: 'mysql://localhost:3307/projetointegrador',
+    },
+  },
+});
+
 const app = express();
 
 app.use(express.json());
 
 app.get('/usuarios', async (req, res) => {
-  const usuarios = await prisma.user.findMany();
+  const usuarios = await prisma.usuario.findMany();
   res.json(usuarios);
 });
 
-
-
-app.listen(3000, () => {
-  console.log('API está rodando em http://localhost:3000');
+app.listen(3307, () => {
+  console.log('API está rodando em http://localhost:3307');
 });
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
